8564dcd27e4a0547e61ef6e99b91dc30b8ebb87b
Thousif Ameer khan <thousifameerkhan@gmail.com>
2023-05-06 12:24:07 +0000

    setup 2

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+
+
 def main():
+    load_dotenv()
     print('Hello World')
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask yout PDF 😁")
+
+    pdf = st.file_uploader("Upload your PDF",type="pdf")
+    # print(os.getenv("OPENAI_API_KEY"))
 
 if __name__ == '__main__':
     main()
