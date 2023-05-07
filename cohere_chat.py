3af609451f73abf4c8771d924aeb4f5a6c18ae31
rahgadda <gaddam.rahul@gmail.com>
2023-05-07 15:41:54 +0000

    Initial Draft

--- cohere_chat.py
+++ cohere_chat.py
@@ -3,3 +3,19 @@ import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+
+def main():
+    print('Starting Document ChatBot')
+
+    # Creating Page
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 😁")
+    pdf = st.file_uploader("Upload your PDF",type="pdf")
+
+    # Loading API Key
+    load_dotenv()
+    print('Loaded Cohere API Key ->'+os.getenv("COHERE_KEY"))
+    print('Loaded Weaviate API Key ->'+os.getenv("WEAVIATE_KEY"))
+
+if __name__ == '__main__':
+    main()
