ad86c0f3dc71513d499aca4a37f31fa0a08e1b49
rahgadda <gaddam.rahul@gmail.com>
2023-05-08 05:02:17 +0000

    Initial Draft

--- cohere_chat.py
+++ cohere_chat.py
@@ -6,6 +6,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import CohereEmbeddings
 from langchain.vectorstores import Weaviate
+import weaviate
 
 def main():
     print('Starting Document ChatBot')
@@ -14,11 +15,10 @@ def main():
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 😁")
     pdf = st.file_uploader("Upload your PDF",type="pdf")
-    user_question = st.text_input("Ask any question about your PDF :")
 
     # Loading API Key
     load_dotenv()
-    print('Loaded Cohere API Key ->'+os.getenv("COHERE_KEY"))
+    print('Loaded Cohere API Key ->'+os.getenv("COHERE_API_KEY"))
     print('Loaded Weaviate API Key ->'+os.getenv("WEAVIATE_URL"))
 
     # Reading PDF
@@ -38,10 +38,59 @@ def main():
         )
         chunks = text_splitter.split_text(text)
 
-        # Creating Weaviate Client
-        # WEAVIATE_URL = getpass.getpass(os.getenv("WEAVIATE_URL"))
-        # knowledgeBase = Weaviate.
+        # Create Embeddings
+        embeddings = CohereEmbeddings()
         
+        # Creating Weaviate Client
+        WEAVIATE_URL = os.getenv("WEAVIATE_URL")
+        client = weaviate.Client(
+            url=WEAVIATE_URL,
+            additional_headers={
+                'X-Cohere-Api-Key': os.getenv("COHERE_API_KEY")
+            })
+        
+        # Delete all Schema
+        client.schema.delete_all()
+        client.schema.get()
+
+        # Create new Schema
+        schema = {
+                    "classes": [
+                        {
+                            "class": "UserManual",
+                            "description": "OFSLL User Manual",
+                            "vectorizer": "text2vec-cohere",
+                            "vectorIndexConfig": {
+                                "distance": "dot"
+                            },
+                            "moduleConfig": {
+                                "text2vec-cohere": {
+                                "model": "multilingual-22-12",
+                                "truncate": "RIGHT"
+                                }
+                            },
+                            "properties": [
+                                {
+                                    "name": "content",
+                                    "dataType": [
+                                        "text"
+                                    ],
+                                    "description": "Vectorized data of OFSLL User Manual",
+                                    "moduleConfig": {
+                                        "text2vec-cohere": {
+                                        "skip": false,
+                                        "vectorizePropertyName": false
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+        client.schema.create(schema)
+        vectorstore = Weaviate(client, "UserManual", "content")
+
+        user_question = st.text_input("Ask any question about your PDF :")
 
 if __name__ == '__main__':
     main()
